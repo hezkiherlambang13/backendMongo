74825553fba8d59516c939f2e048bb837c748276
Hezki Herlambang <hezkih13@gmail.com>
2025-12-12 08:35:06 +0700

    first commit

---
+++ src/pages/ProductDetails.js
@@ -0,0 +1,10 @@
+import { useParams } from "react-router-dom";
+export default function ProductsDetails() {
+const { id } = useParams();
+return (
+<section className="page-section">
+    <h2>Detail Produk #{id}</h2>
+    <p>Informasi lengkap produk akan tampil di sini.</p>
+</section>
+);
+}
